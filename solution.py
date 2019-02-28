c95fbe9017eb9a21145c9f7aa6ad56c7127c1ee4
Anthony Catterwell <anthony.catterwell@gmail.com>
2019-02-28 17:43:52 +0000

    add imports

---
+++ solution.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+from numpy import *
+
+
+def Main():
+    return
+
+def readInput():
+    filename = sys.argv[1]
+    f = open(filename)
+    s = f.readline()
+    f.close();
+
+if __name__ == '__main__':
+    Main()
+
